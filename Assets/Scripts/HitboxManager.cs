80a39fa22f94cd6b3ddce5e2e3c7959ebde38d7a
thewholehamdamily <furusethc@gmail.com>
2018-04-02 11:12:35 -0400

    Some work on hitboxes

--- Assets/Scripts/HitboxManager.cs
+++ Assets/Scripts/HitboxManager.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class HitboxManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public int damage;
+	public int knockback;
+	BoxCollider2D hb;
+
+
+	void OnTrigger2DEnter(Collider2D other)
+	{
+		if (other.tag == "Enemy") {
+			damage (other);
+		}
+		else if (other.tag == "Player") {
+			damage (other);
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }
